54b0d2db826a739a37670270c0659b4f351902c3
Nim <areuschle1@gmail.com>
2019-06-15 12:25:17 -0500

    removed some magic numbers

--- src/client/module/GuiContainer/uiComponents/Clock.lua
+++ src/client/module/GuiContainer/uiComponents/Clock.lua
@@ -15,16 +15,20 @@ local SECS_IN_MIN = 60
 local SECS_IN_HOUR = SECS_IN_MIN*60
 local HOURS_ON_FACE = 12
 
+local CLOCK_SIZE = 256
+local ARM_PADDING = 6
+local HOUR_HAND_RATIO = 1/2
+
 local function hourMarks(numHours)
     local marks = {}
     for i = 1,numHours do
         local theta = (i/numHours)*math.pi*2
-        local posX = math.sin(theta)*60
-        local posY = math.cos(theta)*60
+        local posX = math.sin(theta)*(CLOCK_SIZE/2)
+        local posY = math.cos(theta)*(CLOCK_SIZE/2)
         marks["hour_"..i] = Roact.createElement("Frame", {
             AnchorPoint = Vector2.new(0.5,0.5),
             Position = UDim2.new(0.5,posX+1,0.5,posY+1),
-            Size = UDim2.new(0,3,0,3),
+            Size = UDim2.new(0,4,0,4),
 
             BackgroundColor3 = Color3.fromRGB(31,31,31),
             BorderSizePixel = 0,
@@ -70,9 +74,9 @@ local function hands(t)
     local minHandTheta = minOfHour * math.pi * 2
     local hourHandTheta = hourOfDay * math.pi * 2
 
-    local secHand = clockHand(secHandTheta,60, 2,Color3.fromRGB(180,40,40))
-    local minHand = clockHand(minHandTheta,60)
-    local hourHand = clockHand(hourHandTheta,30)
+    local secHand = clockHand(secHandTheta,(CLOCK_SIZE/2)-ARM_PADDING, 2,Color3.fromRGB(180,40,40))
+    local minHand = clockHand(minHandTheta,(CLOCK_SIZE/2)-ARM_PADDING)
+    local hourHand = clockHand(hourHandTheta,((CLOCK_SIZE/2)-ARM_PADDING)*HOUR_HAND_RATIO)
 
     return Roact.createElement("Frame", {
         Size = UDim2.new(1,0,1,0),
@@ -95,9 +99,8 @@ function Clock:init(initialProps)
 end
 
 function Clock:render()
-
     local clockFrameProps = Dictionary.join(self.props, {
-        Size = UDim2.new(0,128,0,128),
+        Size = UDim2.new(0,CLOCK_SIZE,0,CLOCK_SIZE),
 
         BorderSizePixel = 0,
         BackgroundTransparency = 1
