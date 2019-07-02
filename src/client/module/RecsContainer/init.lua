6c302e5ab622c27fa622d33ade5ba60a04280c1a
Nim <areuschle1@gmail.com>
2019-07-02 14:46:59 -0500

    corrected typo

--- src/client/module/RecsContainer/init.lua
+++ src/client/module/RecsContainer/init.lua
@@ -2,7 +2,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local common = ReplicatedStorage:WaitForChild("common")
 local lib = ReplicatedStorage:WaitForChild("lib")
-local recsPlugins = common:WaitForChild("recsPlugins")
+local recsPlugins = common:WaitForChild("recsplugins")
 
 local RECS = require(lib:WaitForChild("RECS"))
 local PizzaAlpaca = require(lib:WaitForChild("PizzaAlpaca"))
